60d90d4a4ab33b41aceeb1424611306a2c40ad3d
Orenikoe <orental21@gmail.com>
2022-10-13 14:22:03 +0100

    recent commit

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,25 +1,60 @@
-import { useMemo } from "react";
-import { GoogleMap, useLoadScript, MarkerF } from "@react-google-maps/api";
-import './Map-style.css'
+import { useMemo, useState } from 'react';
+import {
+	GoogleMap,
+	useLoadScript,
+	MarkerF,
+	InfoWindow,
+} from '@react-google-maps/api';
+import './Map-style.css';
+import { studentsData } from '../data';
 
- function Home() {
-  const { isLoaded } = useLoadScript({
-    googleMapsApiKey: "AIzaSyAz4T6y4tt_IU8o-ngId8z0lO-wlwOpOrs",
-  });
+function Home() {
+	const { isLoaded } = useLoadScript({
+		googleMapsApiKey: 'AIzaSyAz4T6y4tt_IU8o-ngId8z0lO-wlwOpOrs',
+	});
 
-  if (!isLoaded) return <div>Loading...</div>;
-  return <Map />;
+	if (!isLoaded) return <div>Loading...</div>;
+	return <Map />;
 }
 
 function Map() {
-  const center = useMemo(() => ({ lat: 44, lng: -80 }), []);
+	const center = useMemo(() => ({ lat: 38, lng: -9 }), []);
+	const [markerData, setMarkerData] = useState({});
 
-  return (
-    <GoogleMap zoom={10} center={center} mapContainerClassName="map-container">
-      <MarkerF position={center} />
-    </GoogleMap>
-  
-  );
+	function markerClicked(student) {
+		let { isClicked } = student;
+		if (isClicked === false) {
+			setMarkerData({
+				...student,
+				isClicked: true,
+			});
+		} else if (isClicked === true) {
+			setMarkerData({
+				...student,
+				isClicked: false,
+			});
+		}
+
+	
+	}
+
+
+
+	return (
+		<GoogleMap zoom={10} center={center} mapContainerClassName="map-container">
+			{studentsData.map((student) => {
+				return (
+					<MarkerF
+						key={student.id}
+						position={student.homeAddress}
+						onClick={() => markerClicked(student)}
+					>
+					<InfoWindow position={student.homeAddress}><div>hi</div></InfoWindow>
+					</MarkerF>
+				);
+			})}
+		</GoogleMap>
+	);
 }
 
 export default Home;
---
+++ src/data.js
@@ -0,0 +1,99 @@
+
+
+export const studentsData = [
+    {
+        id: '1',
+        name: 'oren tal',
+        phone: '0546754589',
+        homeAddress: { lat: 38.728020, lng: -9.140930 },
+        isAvailable: false, 
+        isClicked: false
+
+    },
+    {
+        id: '2',
+        name: 'amanda barata',
+        phone: '0546754589',
+        homeAddress: { lat: 38.717140, lng: -9.145520 },
+        isAvailable: true, 
+        isClicked: false
+
+
+    },
+    {
+        id: '3',
+        name: 'andre macedo',
+        phone: '0546754589',
+        homeAddress: { lat: 38.781832, lng: -9.523177 },
+        isAvailable: false, 
+        isClicked: false
+
+    },
+    {
+        id: '4',
+        name: 'andreas leoncedis',
+        phone: '0546754589',
+        normalAddress: 'rua ferreira lapa 27',
+        homeAddress: { lat: 38.727010, lng: -9.143940 },
+        isAvailable: true, 
+        isClicked: false
+
+    },
+    {
+        id: '5',
+        name: 'carla goncalves',
+        phone: '0546754589',
+        homeAddress: { lat: 38.781832, lng: -9.523177 },
+        isAvailable: false, 
+        isClicked: false
+
+    },
+    {
+        id: '6',
+        name: 'goncalo coelho',
+        phone: '0546754589',
+        normalAddress: "rua de olivenca 67",
+        homeAddress: { lat: 38.738892, lng: -9.122393 },
+        isAvailable: false, 
+        isClicked: false
+
+    },
+    {
+        id: '7',
+        name: 'goncalo da silva',
+        phone: '0546754589',
+        homeAddress: { lat: 38.781832, lng: -9.523177 },
+        isAvailable: true, 
+        isClicked: false
+
+    },
+    {
+        id: '8',
+        name: 'nuno correia',
+        phone: '0546754589',
+        normalAddress: 'rua doutor jorge munoz cardoso 1',
+        homeAddress: { lat: 38.642880, lng: -9.094380 },
+        isAvailable: false, 
+        isClicked: false
+
+    },
+    {
+        id: '9',
+        name: 'raquel lopes',
+        phone: '0546754589',
+        homeAddress: { lat: 38.781832, lng: -9.523177 },
+        isAvailable: false, 
+        isClicked: false
+
+    },
+    {
+        id: '10',
+        name: 'simon thouet',
+        phone: '0546754589',
+        homeAddress: { lat: 38.781832, lng: -9.523177 },
+        isAvailable: false, 
+        isClicked: false
+
+    }
+    
+]
