b72013a620e5dbf3e537cb9d142adeead0d57efe
Orenikoe <orental21@gmail.com>
2022-10-13 09:53:06 +0100

    none

--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -10,15 +10,14 @@ import {
 export const MapContainer = () => {
 	const [infoWindowOpen, setInfoWindowOpen] = React.useState(false);
 
-	const showWindow = () => {
+	const showWindow = (e) => {
 		setInfoWindowOpen(true);
-
 	};
+
 	const closeWindow = () => {
-		
 		setInfoWindowOpen(false);
 	};
-  
+
 	const mapStyles = {
 		height: '50vh',
 		width: '50%',
@@ -33,28 +32,29 @@ export const MapContainer = () => {
 	return (
 		<LoadScript googleMapsApiKey="AIzaSyAz4T6y4tt_IU8o-ngId8z0lO-wlwOpOrs">
 			<GoogleMap mapContainerStyle={mapStyles} zoom={14} center={defaultCenter}>
-
-
 				{studentsData.map((student) => {
-          
 					return (
 						<MarkerF
-            key={student.id}
-           
+							key={student.id}
 							position={student.homeAddress}
-							onClick={() => showWindow()}
+							onClick={(e) => (new google.maps.InfoWindow())}
+							// setInfoWindowOpen(true)
+
+							// }
 						>
-				{infoWindowOpen && (
-					<InfoWindow onCloseClick={(e) => closeWindow(e)}>
-						<p>{student.name} {student.phone} {student.isAvailable ? <p>Let's Do something!</p> : null }</p>
-						{/* <p>{student.phone}</p> */}
-						{/*  */}
-					</InfoWindow>
-				)}
-        </MarkerF>
+							{infoWindowOpen && (
+								<InfoWindow onCloseClick={(e) => closeWindow(e)}>
+									<p>
+										{student.name} {student.phone}{' '}
+										{student.isAvailable ? <p>Let's Do something!</p> : null}
+									</p>
+									{/* <p>{student.phone}</p> */}
+									{/*  */}
+								</InfoWindow>
+							)}
+						</MarkerF>
 					);
 				})}
-
 			</GoogleMap>
 		</LoadScript>
 	);
